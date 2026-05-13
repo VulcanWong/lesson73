31111d19c1e803228ae1a145dce4aed4e0facf2c
VulcanWong <wongvulcan@gmail.com>
2026-05-13 20:11:33 +0800

    lesson73

---
+++ activty2.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+database = 'database.sqlite'
+
+conn = sqlite3.connect(database)
+print('Opened data successfully')
+
+
+
+
+
+
+import pandas as pd
+tables = pd.read_sql("""SELECT * FROM sqlite_master WHERE type='tables';""",conn)
+tables
